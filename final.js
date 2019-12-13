b3d106338580ce679c9fee84f17b2c1ab1cf33eb
Cody Mailey <cmailey03@gmail.com>
2019-12-13 09:52:03 -0700

    online

--- final.js
+++ final.js
@@ -1,6 +1,23 @@
 
 function ready() {
-document.body.style.background = "#EAE7DC";
+    document.body.style.background = "#EAE7DC";
+    // slider variables
+    var slider = document.getElementById("myRange");
+    var output = document.getElementById("demo");
+    output.innerHTML = slider.value;
+
+    //continues to change sliders number if moved
+    slider.oninput = function() {
+        output.innerHTML = this.value;
+        document.getElementById("def").innerHTML=" ";
+        def.append(output.innerHTML);
+    }
+    // swaping images based on slider using if statements
+    if (output = 1) {
+        document.getElementById('map').src = "https://www.euratlas.net/history/europe/1/1.jpg";
+    }else if(output = 100) {
+        document.getElementById('map').src = "https://www.euratlas.net/history/europe/100/100.jpg"
+    }
 
 }
 
